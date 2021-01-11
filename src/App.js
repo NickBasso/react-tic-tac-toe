5e102d701a71d8c915f15d4e17c1f2026e980a7f
Nicolae Basso <nickbassotrue@gmail.com>
2021-01-11 14:53:36 +0200

    Add game's functionality, now it's playable according to the rules

--- src/App.js
+++ src/App.js
@@ -1,35 +1,67 @@
 import React from 'react';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
-
+/*class Square extends React.Component {
   render() {
     return (
       <button 
         className="square" 
-        onClick={() => {
-          this.setState({value: 'X'},
-          () => console.log(this.state.value));
-        }}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
+}*/
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares : Array(9).fill(null),
+      xIsNext : true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares : squares,
+      xIsNext : !this.state.xIsNext});
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    //const status = 'Next player: ' + this.state.xIsNext ? 'X' : 'O';
+
+    return (<Square
+              value={this.state.squares[i]}
+              onClick={() => this.handleClick(i)}  
+            />)
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+console.log(winner);
+    if(winner != null) {
+      
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <React.Fragment>
@@ -68,4 +100,26 @@ export default class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) 
+      return squares[a];
+  }
+
+  return null;
 }
